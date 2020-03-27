c7dfbc69ebe98d0cb2916b0a574ea336d1d69f46
Chris Bouchard <chris@upliftinglemma.net>
2020-03-26 21:37:09 -0400

    Remove old password secrets before creating new ones

--- set-database-password.sh
+++ set-database-password.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+docker secret rm discord-fate-bot-db-password discord-fate-bot-pgpass
+
 password=$(openssl rand -base64 12)
 echo "$password" | docker secret create discord-fate-bot-db-password -
 echo "database:*:*:*:$password" | docker secret create discord-fate-bot-pgpass -
